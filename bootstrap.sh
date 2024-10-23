f2ab673a2d3f673371c807a198f5992850b78984
Sylvain Martin <sylvain@nforcer.com>
2024-10-23 18:14:54 -0400

    naming

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,5 +32,5 @@ kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.passwor
 echo "ArgoCD admin password has been set to: ${adminpassword}"
 
 # Uncomment these lines if you want to port-forward the ArgoCD server to localhost:8080 and/or use kubeseal for secrets management
-# kubectl port-forward -n argocd svc/argocd-server 8080:80
+kubectl port-forward -n argocd svc/argocd-server 8080:80
 # kubeseal --controller-name sealed-secrets --controller-namespace argo-common -o yaml < infile > outfile
