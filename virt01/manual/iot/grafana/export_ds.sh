069ba52df896b84acfde44abc4e2b426686f2d95
Sylvain Martin <sylvain@nforcer.com>
2025-05-19 22:02:22 -0400

    grafana

--- virt01/manual/iot/grafana/export_ds.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mkdir -p data_sources && curl -s "https://grafana.k3s.nf.lab/api/datasources"  -u admin:admin|jq -c -M '.[]'|split -l 1 - data_sources/
--- virt01/manual/iot/grafana/import_ds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in data_sources/*; do \
-    curl -k -X "POST" "https://grafana.apps.ocp.nf.lab/api/datasources" \
-    -H "Content-Type: application/json" \
-     --user admin:G0tR0gu3\! \
-     --data-binary @$i
-done
