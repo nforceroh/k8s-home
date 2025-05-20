5ce7af43b13001ee84095efdd6ef21ca6ff70e8a
Sylvain Martin <sylvain@nforcer.com>
2025-05-20 17:41:20 -0400

    tweak

--- ref/dell/fanspeeds.sh
+++ ref/dell/fanspeeds.sh
@@ -1,3 +1,38 @@
-ipmitool -I lanplus -H 192.168.101.200 -U fans -P fans raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x01 0x00 0x00
-ipmitool -I lanplus -H 192.168.101.200 -U fans -P fans raw 0x30 0x30 0x01 0x00
-ipmitool -I lanplus -H 192.168.101.200 -U fans -P fans raw 0x30 0x30 0x02 0xff 0x14
+#!/bin/bash
+# https://www.dell.com/support/kbdoc/en-dm/000136288/idrac-how-to-set-the-idrac-thermal-base-algorithm-via-cli
+
+IDRAC_USER=fans
+IDRAC_PW=fans
+MAXTEMP=40
+FANSPEED=5
+
+function get_temp {
+  local TEMP=$(ipmitool -I lanplus -H $IDRAC_HOST -U $IDRAC_USER -P $IDRAC_PW sdr type temperature | grep Exhaust | grep -o -e '[0-9][0-9] degrees' | grep -o -e '[0-9][0-9]')
+  echo $TEMP
+}
+
+function set_dynamic {
+  HEX=$(printf '0x%02x' $1)
+  ipmitool -I lanplus -H $IDRAC_HOST -U $IDRAC_USER -P $IDRAC_PW raw 0x30 0x30 0x01 $HEX > /dev/null
+}
+
+function set_speed {
+  HEX=$(printf '0x%02x' $1)
+  ipmitool -I lanplus -H $IDRAC_HOST -U $IDRAC_USER -P $IDRAC_PW raw 0x30 0x30 0x02 0xff $HEX > /dev/null
+}
+
+
+
+for IDRAC_HOST in 192.168.101.200 192.168.101.199; do
+  echo "Setting fan speed for $IDRAC_HOST"
+  TEMP=$(get_temp)
+  if [ $TEMP -gt $MAXTEMP ]; then
+    echo "Exhaust Temperature is $TEMP C - Temperature is too high; using dynamic fan control"
+    set_dynamic 1
+  else
+    echo "Exhaust Temperature is $TEMP C - Temperature is normal; using manual fan control of $FANSPEED %"
+    set_dynamic 0
+    set_speed $FANSPEED
+  fi
+
+done
